0eb26a329978ebfbefde8f47af069870974229a6
jbwalton <johngray3d@googlemail.com>
2021-10-05 12:57:37 +0200

    Update script.js
    completely changed contents on script.js to create  an empty pokemonList array and add elements to it using the .push notation.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favouriteFood = 'Fish and Chips';
-document.write(favouriteFood);
+let pokemonList = [];
+pokemonList.push({name:'Bulbasaur',height:7,type:['grass','poison']});
+pokemonList.push({name:'Caterpie',height:3,type:['bug']});
+pokemonList.push({name:'Rattata',height:3,type:['normal']});
+// to access the array elements in each object in the array use syntax console.log(pokemonList[0].type[1]);   which returns the value 'poison' 
