e1e4152d904d8a514b36c1c32feec864566b1cc4
jbwalton <johngray3d@googlemail.com>
2021-10-06 12:07:00 +0200

    for next looping
    created a for next loop to traverse the array pokemonList and display the names of each pokemon in the list.
    On each loop, the height parameter is tested, and if greater than.7, the comment "wow, thats big" is displayed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 let pokemonList = [];
-pokemonList.push({name:'Bulbasaur',height:7,type:['grass','poison']});
-pokemonList.push({name:'Caterpie',height:3,type:['bug']});
-pokemonList.push({name:'Rattata',height:3,type:['normal']});
-// to access the array elements in each object in the array use syntax console.log(pokemonList[0].type[1]);   which returns the value 'poison' 
+pokemonList.push({name:'Bulbasaur',height:.7,type:['grass','poison']});
+pokemonList.push({name:'Caterpie',height:.3,type:['bug']});
+pokemonList.push({name:'Rattata',height:.3,type:['normal']});
+// to access the array elements in each object in the array use syntax console.log(pokemonList[0].type[1]);   which returns the value 'poison'
+
+for (let i=0; i<=5; i++){
+  document.write(pokemonList[i].name+" (height: "+pokemonList[i].height+") ");
+  if (pokemonList[i].height >=.7) {
+    document.write(" - Wow, that's big <br>");
+  }else{
+      document.write("<br>");
+    }
+  }
