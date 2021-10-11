d9e90ab7841f4d3aeb3b37191fe407d81234b567
jbwalton <johngray3d@googlemail.com>
2021-10-11 13:56:40 +0200

    Update script.js
    changed for for next loop to .foreach and encased the pokemonList in an IIFE to protect it from accidental global modification, adding the functionality .push to add to the list, and .getAll to obtain the entire list.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
-let pokemonList = [];
-pokemonList.push({name:'Bulbasaur',height:.7,type:['grass','poison']});
-pokemonList.push({name:'Caterpie',height:.3,type:['bug']});
-pokemonList.push({name:'Rattata',height:.3,type:['normal']});
-pokemonList.push({name:'Sandslash',height:1,type:['ground']});
-pokemonList.push({name:'Vulpix',height:.6,type:['fire']});
-pokemonList.push({name:'Paras',height:.3,type:['grass','bug']});
-pokemonList.push({name:'Golduck',height:1.7,type:['water']});
-// to access the array elements in each object in the array use syntax console.log(pokemonList[0].type[1]);   which returns the value 'poison'
-
-for (let i=0; i<=pokemonList.length; i++){
-  document.write(pokemonList[i].name+" (height: "+pokemonList[i].height+") ");
-  if (pokemonList[i].height >=.7) {
+//Define function output to write the Pokemon List by item
+function output(item) {
+  document.write(item.name+" (height: "+item.height+") ");
+  if (item.height >=.7) {
     document.write(" - Wow, that's big <br>");
   }else{
-      document.write("<br>");
-    }
+    document.write("<br>");
   }
+};
+
+//Immediately Invoked Function Expression (IIFE)
+let pokemonRepository = (function() {
+  let pokemonList = [
+  {name:'Bulbasaur',height:.7,type:['grass','poison']},
+  {name:'Caterpie',height:.3,type:['bug']},
+  {name:'Rattata',height:.3,type:['normal']},
+  {name:'Sandslash',height:1,type:['ground']},
+  {name:'Vulpix',height:.6,type:['fire']},
+  {name:'Paras',height:.3,type:['grass','bug']},
+  {name:'Golduck',height:1.7,type:['water']}
+  ];
+
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+    function getAll() {
+      return pokemonList;
+    }
+    return {
+      add: add,
+      getAll: getAll,
+    };
+  })();
+
+//Output PokemonList using the function output
+(pokemonRepository.getAll()).forEach(output);
