c3d3fe4bf2810338bacc845cbc587ff03aed0a3f
jbwalton <johngray3d@googlemail.com>
2021-10-14 11:41:30 +0200

    Update script.js
    added addListItem function to the pokemon repository to populate the unorder list with buttons containing the pokemon names, with a click event listener that lists the attributes of the pokemon in the console.

--- js/script.js
+++ js/script.js
@@ -28,11 +28,30 @@ let pokemonRepository = (function() {
     function getAll() {
       return pokemonList;
     }
+    function addListItem(pokemon){
+      let pokemonList=document.querySelector('.pokemon-list');
+      let listpokemon=document.createElement('li');
+      let button=document.createElement('button');
+      button.innerText=pokemon.name;
+      button.classList.add('button-class');
+      listpokemon.appendChild(button);
+      pokemonList.appendChild(listpokemon);
+      button.addEventListener('click', function(){
+        showDetails(pokemon)
+      });
+    }
+
+    function showDetails(pokemon){
+      console.log(pokemon);
+    }
     return {
       add: add,
       getAll: getAll,
+      addListItem: addListItem,
     };
   })();
 (pokemonRepository.add({name:'Kadabra',height:1.3,type:['psychic']}));
 //Output PokemonList using the function output
-(pokemonRepository.getAll()).forEach(output);
+(pokemonRepository.getAll()).forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
+});
